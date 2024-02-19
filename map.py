0c8e2a1c33564f1233a039689c9a54b8a34f0f43
User <danielyangevg@yandex.ru>
2024-02-19 18:02:22 +0300

    Add Map class

--- map.py
+++ map.py
@@ -1,2 +1,26 @@
+class Map:
 
-class Map(): pass
+    def __init__(self, length, width):
+        self.width = width
+        self.length = length
+        self.field = {row: [self.Cell(row, column) for column in range(width)] for row in range(length)}
+
+    class Cell:
+
+        def __init__(self, x, y, content=None):
+            self.content = content
+            self.y = y
+            self.x = x
+
+        def put(self, what):
+            self.content = what
+
+        def pop(self):
+            obj = self.content
+            self.content = None
+            return obj
+
+        def __str__(self):
+            return f'A cell with {self.content} and {self.x, self.y} coordinates'
+
+
