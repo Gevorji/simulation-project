cf9d3ce5839be9c2f8d0e96cd2941c07fa3d8fd8
Gevorji <techno9882@gmail.com>
2024-02-21 21:40:36 +0300

    Work on renderer class

--- renderer.py
+++ renderer.py
@@ -1,20 +1,20 @@
-class BaseRectangleFieldRenderer:
-    
+from map import Map
+
+class BaseRectangleFieldRenderer(Map):
+
     def __init__(self, rend_obj, layout_mappings):
+        super().__init__(rend_obj.length, rend_obj.width)
+        # an assertion for all keys of layout mappings to be type class instances
         self.layout_mappings = layout_mappings
         self.rend_obj = rend_obj
-        self.field = [[None]*self.rend_obj.width for row in range(self.rend_obj.length)]
-        self.layout = self.render()
         
     def render(self):
-        for obj in self.rend_obj.field_iterator(): # walks through field and updates self field state
-            pass                                   # expects renderable object to have a method field_iterator.
+        for cell, obj in zip(self.field_iterator(), self.rend_obj.field_iterator()): # walks through field and updates self field state
+            mapped_obj = self.layout_mappings[type(obj)]
+            cell.put(mapped_obj)                                   # expects renderable object to have a method field_iterator.
                                                    # A field iterator returns objects
                                                    # that renderable objects contains in a strict order: from left upper
                                                    # corner to the bottom right corner
 
 
-    def display(self): pass # returns self.view
 
-
-    
