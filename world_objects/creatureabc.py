94a67aeb6deb1ab810807ca2321c76f8c7dd7ac2
Gevorji <techno9882@gmail.com>
2024-03-03 21:47:16 +0300

    A bit of work on creature abstract class

--- world_objects/creatureabc.py
+++ world_objects/creatureabc.py
@@ -1,3 +1,5 @@
+from abc import abstractmethod
+
 try:
     from .entityabc import Entity
 except ImportError:
@@ -5,15 +7,20 @@ except ImportError:
 
 
 class Creature(Entity):
-    def __init__(self, health_points, move_speed):
+    def __init__(self, health_points, move_speed, environment):
+        self.environment = environment
         self.health_points = health_points
         self.move_speed = move_speed
 
+    @abstractmethod
     def make_move(self): pass
 
     @property
     def visible_area(self): pass # a collection of cells that creature can see
 
-    def take_damage(self): pass
+    def take_damage(self, damage):
+        self.health_points -= damage
 
     def wander(self): pass
+
+if __name__ == '__main__': pass
