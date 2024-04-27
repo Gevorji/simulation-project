decafd1ff3fa8af43c32f9cb5552e0be4b416ad7
Gevorji <techno9882@gmail.com>
2024-04-27 16:54:42 +0300

    Fix grass class

--- world_objects/grass.py
+++ world_objects/grass.py
@@ -4,8 +4,9 @@ from .entityabc import Entity
 class Grass(Entity):
 
     def __init__(self, health_points, hp_restore):
-        self.health_points = health_points
+        super().__init__(health_points)
         self.hp_restore = hp_restore
 
     def consume(self):
-        self.health_points -= 1
+        self.take_damage(1)
+        return self.hp_restore
