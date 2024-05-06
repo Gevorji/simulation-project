00ab1e6810d5288b5681c4c9d54237058b16fece
Gevorji <techno9882@gmail.com>
2024-05-06 18:04:02 +0300

    Rework entrie classes and make_presentational_format

--- simulationlogger.py
+++ simulationlogger.py
@@ -7,20 +7,33 @@ from world_objects.actions import Actions
 @dataclasses.dataclass
 class ActionEntry:
     action: Actions | None
-    actor: Cell
-    target: Cell | None
+    _actor: Cell
+    _target: Cell | None
+
+    def __post_init__(self):
+        self.actor_pos = self._actor.x, self._actor.y
+        self.actor = getattr(self._actor.content, 'name', '(nameless)')
+        self.target = 'cell' if not self._target.content else (
+            getattr(self._target.content, 'name', self._target.content.__class__.__name__))
+        self.target_pos = self._target.x, self._target.y
+        self.act_type = {Actions.ATTACK: 'attack', Actions.EAT: 'eating', Actions.MOVE: 'move'}[self.action.type]
 
 
 @dataclasses.dataclass
 class EntityStateEntry:
-    entity: Cell
+    cell: Cell
     hp: int
+
+    def __post_init__(self):
+        self.entity = getattr(self.cell.content, 'name', self.cell.content.__class__.__name__)
+        self.entity_pos = self.cell.x, self.cell.y
 
 
 @dataclasses.dataclass
 class RestorationObjectsEntry:
     object_type: str
     how_much: int
+
 
 class Logger:
     def __init__(self):
@@ -48,17 +61,13 @@ def make_presentational_format(entry):
         action = entry.action
         actor = entry.actor
         target = entry.target
-        actor_name, = getattr(actor.content, 'name', '(nameless)'),
-        target_name = getattr(target.content, 'name', '(nameless)')
         if action is None:
-            return f'{actor_name} (pos {actor.x, actor.y}) does nothing'
-        act_type = {Actions.ATTACK: 'attack', Actions.EAT: 'eating', Actions.MOVE: 'move'}[action.type]
-        return f'{actor_name} (pos {actor.x, actor.y}) makes {act_type} to {target_name} (pos {target.x, target.y}'
+            return f'{entry.actor} (pos {entry.actor_pos}) does nothing'
+        return f'{entry.actor} (pos {entry.actor_pos}) makes {entry.act_type} to {entry.target} (pos {entry.target_pos})'
 
     if isinstance(entry, EntityStateEntry):
-        entity = entry.entity
-        entity_name = getattr(entity.content, 'name', '(nameless)')
-        return f'{entity_name} (pos {entity.x, entity.y}) state: hp={entry.hp}'
+        cell = entry.entity
+        return f'{entry.entity} (pos {entry.entity_pos}) state: hp={entry.hp}'
 
     if isinstance(entry, RestorationObjectsEntry):
         return f'Restoring {entry.object_type}: {str(int(entry.how_much))} units'
